44528b69458b870d522ab7eec4eadb7bd4b77ba1
Kings11b <king1kings99@gmail.com>
2024-11-01 19:57:02 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: 'My Bot',
   botName: "Knight Bot",
   botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  ownerNumber: '491783757571', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
